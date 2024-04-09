9bb35afaf3625d1bb656173f1be73873b0b455c7
Ana Škarica <ana.skarica.as@gmail.com>
2024-04-09 13:59:06 +0200

    Add color palette, default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ligth-blue": "#E8F1F5",
+        "grey-blue": "#3F4344",
+        "main-blue": "#4A96DC",
+        "dark-blue": "#3F4344",
+        "middle-blue": "#BBD5ED",
+      },
+    },
   },
   plugins: [],
 };
